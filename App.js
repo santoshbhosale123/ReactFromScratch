1c5603b73c8f1682990eb7c93a104092b93c35d3
Santosh Bhosale <santoshb@inboxops.local>
2023-08-25 01:54:56 +0530

    nested structure

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
 
-        const heading = React.createElement("h1",{id:"heading",zyz:"abc"},"Hello santosh");
+
+          const parent = React.createElement(
+            "div",{id:"parent"},
+            [React.createElement("div",{id:"child"},[
+                React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")
+            ]
+            ),React.createElement("div",{id:"child2"},[
+                React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")
+            ]
+            )]
+            );
+          console.log(parent)
+        //const heading = React.createElement("h1",{id:"heading",zyz:"abc"},"Hello santosh");
         const root1 = ReactDOM.createRoot(document.getElementById("root"));
-        root1.render(heading)
+        root1.render(parent)
+
+        
         
