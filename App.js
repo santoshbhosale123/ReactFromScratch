103e8111051ce02c0110345b4fdd30ee01e98ef7
Santosh Bhosale <santoshb@inboxops.local>
2023-08-25 01:35:00 +0530

    css

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 
-        const heading = React.createElement("h1",{},"Hello santosh");
+        const heading = React.createElement("h1",{id:"heading",zyz:"abc"},"Hello santosh");
         const root1 = ReactDOM.createRoot(document.getElementById("root"));
         root1.render(heading)
         
