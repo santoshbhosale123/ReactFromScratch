47e33d942fb0d637df35e18c09264f22df45404e
Santosh Bhosale <santoshb@inboxops.local>
2023-08-25 01:23:37 +0530

    js seperation

---
+++ App.js
@@ -0,0 +1,5 @@
+
+        const heading = React.createElement("h1",{},"Hello santosh");
+        const root1 = ReactDOM.createRoot(document.getElementById("root"));
+        root1.render(heading)
+        
