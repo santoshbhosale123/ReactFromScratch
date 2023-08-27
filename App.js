e84a58e9231614bf065c20bf22dcbd547d1ad2ae
Santosh Bhosale <santoshb@inboxops.local>
2023-08-27 21:43:02 +0530

    2nd Episode

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 
-
+import React from "react";
+import ReactDOM  from "react-dom/client";
           const parent = React.createElement(
             "div",{id:"parent"},
             [React.createElement("div",{id:"child"},[
